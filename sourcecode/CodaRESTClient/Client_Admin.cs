a4d6fe50dcde04986221a16d7c74707a88ab25d6
Jay Imerman <jimerman@gmail.com>
2023-01-16 11:05:24 -0500

    Update client to latest version; added Paid Subscription Plan, Admin methods, and logo file

---
+++ sourcecode/CodaRESTClient/Client_Admin.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace CodaRESTClient
+{
+    public partial class Client
+    {
+        /// <summary>
+        /// Queues up a bulk email to go out to various Networks
+        /// </summary>
+        /// <param name="Subject"></param>
+        /// <param name="Body"></param>
+        /// <param name="ToNetworks"></param>
+        /// <returns></returns>
+        public JObject SendBulkMail(string Subject, string Body, string[] ToNetworks)
+        {
+            var request = NewRequest("/api/admin/email/network", Method.Post);
+            request.AddHeader("subject", Subject);
+            request.AddHeader("networks", string.Join(",", ToNetworks));
+            request.AddBody(Body);
+            return GetResponse(request);
+        }
+
+        /// <summary>
+        /// Retrieves an alternate Account ID by type
+        /// </summary>
+        /// <param name="AccountId">CodaEA Account ID</param>
+        /// <param name="AlternateIdName">Alternate ID name (e.g. Stripe, Discord...)</param>
+        /// <returns></returns>
+        public JObject GetAccountAlternateKey(long AccountId, string AlternateIdName)
+        {
+            var request = NewRequest($"/api/account/{AccountId}/alternateId", Method.Get);
+            request.AddHeader("systemname", AlternateIdName); 
+            return GetResponse(request);
+        }
+
+        /// <summary>
+        /// Set an alternate Account ID by type
+        /// </summary>
+        /// <param name="AccountId">CodaEA Account ID</param>
+        /// <param name="AlternateIdName">Alternate ID name</param>
+        /// <param name="AlternateId">Alternate ID value</param>
+        /// <param name="TimeToLiveMinutes">How long before the ID expires; if not specified, default is 20 years</param>
+        /// <returns></returns>
+        public JObject SetAccountAlternateKey(long AccountId, string AlternateIdName, string AlternateId, Int32 TimeToLiveMinutes = -1)
+        {
+            var request = NewRequest($"/api/account/{AccountId}/alternateId", Method.Put);
+            request.AddHeader("systemname", AlternateIdName);
+            request.AddHeader("alternateid", AlternateId);
+            if (TimeToLiveMinutes > 0)
+            {
+                request.AddHeader("altidttl", TimeToLiveMinutes.ToString());
+            }
+            return GetResponse(request);
+        }
+    }
+}
---
+++ sourcecode/CodaRESTClient/Client_PSP.cs
@@ -0,0 +1,50 @@
+﻿/***************************************************
+ * This file deals with functions specifically related to Paid Subscription Plans
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace CodaRESTClient
+{
+    public partial class Client
+    {
+        /// <summary>
+        /// Retrieves a specific Paid Subscription Plan
+        /// </summary>
+        /// <param name="SubscriptionId"></param>
+        /// <returns></returns>
+        public JObject GetPSP(long SubscriptionId)
+        {
+            var request = NewRequest($"/api/psp/planid/{SubscriptionId}", RestSharp.Method.Get);
+            return GetResponse(request);
+        }
+
+        /// <summary>
+        /// Retrieves an array of PSPs that grant access to the given Network
+        /// </summary>
+        /// <param name="Network">Network code, or use "all" to retrieve plans for all networks</param>
+        /// <returns></returns>
+        public JArray GetPSPsForNetwork(string Network)
+        {
+            var request = NewRequest($"/api/psp/plans/{Network}", RestSharp.Method.Get);
+            return GetResponseArray(request);
+        }
+
+        /// <summary>
+        /// Assigns a Paid Subscription Plan to an Account (assuming payment has been processed)
+        /// </summary>
+        /// <param name="AccountId"></param>
+        /// <param name="SubscriptionId"></param>
+        /// <returns></returns>
+        public JObject SubscribeAccountToPSP(long AccountId, long SubscriptionId)
+        {
+            var request = NewRequest($"/api/account/{AccountId}/subscribepsp/{SubscriptionId}", Method.Put);
+            return GetResponse(request);
+        }
+    }
+}
